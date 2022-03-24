1860873a3eb112873300cb7d685ef9e65f39818f
billytesterpullscription <102319636+billytesterpullscription@users.noreply.github.com>
2022-03-24 11:19:53 -0700

    initial commit

---
+++ JSON nil tester/JSON nil tester/JSON_nil_testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_nil_testerApp.swift
+//  JSON nil tester
+//
+//  Created by William Welch on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct JSON_nil_testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
